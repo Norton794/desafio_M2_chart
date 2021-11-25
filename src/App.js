416abffd15b7b35fd87c0c8fdaca44e713fffd33
Norton dos Santos <nortonsantos79@gmail.com>
2021-11-25 17:35:12 -0300

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className={`pl-5 bg-gradient-to-r from-blue-500 to-purple-500
+    text-white`} >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
