4359dcf966c55049bb3a4112499f98ccfc2e872c
Norton <nortonsantos79@gmail.com>
2021-11-25 21:52:36 -0200

    db.json

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Chart from '../components/Chart'
+function App() {
+  return (
+    <div className="App">
+      <Chart/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Chart.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Chart(){
+const initialState = {
+    chart: [],
+    productsTypeOne: [],
+    productsTypeTwo: [],
+    productsTypeThree: [],
+    total: 0.0
+}
+    return(
+        <div>
+        
+        </div>
+    )
+}
