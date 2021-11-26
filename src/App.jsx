108ad765d71d2f6262cf4f8ea6ab18d6cea20804
Norton dos Santos <nortonsantos79@gmail.com>
2021-11-26 14:31:14 -0300

    saving reorganization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 import Chart from './components/Chart'
 function App() {
+=======
+import './App.css';
+
+export default function App() {
+>>>>>>> c3547df (reorganized the db.json)
   return (
-    <div className="App">
-      <Chart/>
+    <div>
+      
     </div>
   );
 }
 
-export default App;
